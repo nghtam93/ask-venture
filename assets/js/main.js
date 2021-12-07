ba8644a6e6c6a20792fa52571433531d0d910c54
doannguyen263 <doannguyen263@gmail.com>
2021-12-07 21:48:43 +0700

    Update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,17 @@
 $(document).ready(function(){
 
+     new WOW().init();
+
     /*----Get Header Stick ---*/
     var header_sticky=$("header.-fix")
     $(window).scroll(function(){
         $(this).scrollTop()>5?header_sticky.addClass("is-active"):header_sticky.removeClass("is-active")
     })
+
     $( window ).on( "load", function() {
         (header_sticky.offset().top >5) ? header_sticky.addClass("is-active"):header_sticky.removeClass("is-active")
     });
+
 
     /*----Languages---*/
     $('.languages .languages-item').click(function() {
@@ -95,101 +99,36 @@ $(document).ready(function(){
         return o.preventDefault(),$("html, body").animate({scrollTop:0},duration),!1
     });
 
-    $('button[data-bs-toggle="tab"]').on('shown.bs.tab', function (e) {
-      $('.js-slick').slick('setPosition');
-    })
 
     //check home
     if($('body').hasClass( "home" )){
 
-        // Switch
-        $('.js-switch').on("click",function(e) {
-
-            var stt = $(this).data('stt')
-            if(stt =='on'){
-                $('.js-switch-button input').prop('checked', true);
-
-            }else{
-                $('.js-switch-button input').prop('checked', false);
-            }
-        })
-
-        $('.js-switch-button input').on("click",function(e) {
-            var firstTabEl = document.querySelector('.js-switch-1 a')
-            var secondTabEl = document.querySelector('.js-switch-2 a')
-            var firstTab = new bootstrap.Tab(firstTabEl)
-            var secondTab = new bootstrap.Tab(secondTabEl)
 
 
-            if ($('.js-switch-button input:checked').val() !== undefined){
-                firstTab.show()
-            }else{
-                secondTab.show()
-            }
-        })
-
-        new WOW().init();
-
-        $('.home-team__slider').slick({
-            infinite: false,
-            slidesToShow: 1,
+    }
+    if($('body').hasClass( "single" )){
+        $('.related__slider').slick({
+            slidesToShow: 3,
             slidesToScroll: 1,
-            arrows: false,
-            dots: true,
-            easing: "linear",
-        });
-
-        $('.home-partner__slider').slick({
-            rows: 2,
-            infinite: false,
-            slidesToShow: 10,
-            slidesToScroll: 4,
             arrows: true,
             dots: false,
-            easing: "linear",
-            prevArrow: '<span class="icon-arrow-left slick-prev slick-arrow"></span>',
-            nextArrow: '<span class="icon-arrow-right slick-next slick-arrow"></span>',
             responsive: [
-                {
-                  breakpoint: 991,
-                  settings: {
-                    slidesToShow: 6,
-                    slidesToScroll: 4,
-                    dots: false,
-                    arrows: true
-                  }
-                },
                 {
                   breakpoint: 767,
                   settings: {
-                    slidesToShow: 4,
-                    slidesToScroll: 4,
-                    dots: false,
-                    arrows: true
+                    slidesToShow: 2,
                   }
                 },
                 {
                   breakpoint: 575,
                   settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 2,
-                    dots: false,
-                    arrows: true
+                    centerMode: true,
+                    variableWidth: true,
+                    slidesToShow: 1,
                   }
                 }
             ]
         });
-
-    }
-    if($('body').hasClass( "single" )){
-        var single_share = $('.single__share')
-        var share_offset = $('.single__share').offset().top
-
-        $(window).scroll(function(){
-            $(this).scrollTop()>share_offset?single_share.addClass("is-active"):single_share.removeClass("is-active")
-        })
-
-        console.log(single_share)
     }
 
 });
